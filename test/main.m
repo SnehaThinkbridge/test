779f25612d3d9b98580e75e34a463d5ca40b8dbb
ThinkBridge <thinkbridge@ThinkBridges-Mac-mini.local>
2014-10-01 17:23:16 +0530

    Initial Commit

---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by ThinkBridge on 10/1/14.
+//  Copyright (c) 2014 ThinkBridge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
